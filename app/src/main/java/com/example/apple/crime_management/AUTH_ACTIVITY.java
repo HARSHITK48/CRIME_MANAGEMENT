8bf9da9261699dcb17a5bf4a108af3cfc31709b1
apple <apple@apples-MacBook-Air.local>
2018-08-13 18:16:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/apple/crime_management/AUTH_ACTIVITY.java
@@ -0,0 +1,13 @@
+package com.example.apple.crime_management;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AUTH_ACTIVITY extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth__activity);
+    }
+}
